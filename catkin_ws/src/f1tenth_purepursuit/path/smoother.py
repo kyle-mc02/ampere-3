dff63321a3bf95c8373a8b7d31ed28bf84616d3f
John-Zoscak-02 <jzoscak02@gmail.com>
2023-12-11 13:01:12 -0500

    command line arg for sigma and automatic loop closure

--- catkin_ws/src/f1tenth_purepursuit/path/smoother.py
+++ catkin_ws/src/f1tenth_purepursuit/path/smoother.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
-import scipy as sci
+from scipy.ndimage import gaussian_filter1d
+#from scipy.ndimage import gaussian_laplace
 
 df = pd.read_csv("%s.csv"%(sys.argv[1]), header=None)
 print(df)
 
+df = pd.concat([df, df.loc[:7]], ignore_index=True)
+
 x, y = df.loc[:, 0], df.loc[:, 1]
+xy = df.loc[:, 0:1]
 #print(x)
 #print(y)
 
-xp, yp = sci.ndimage.gaussian_filter1d(x, 4), sci.ndimage.gaussian_filter1d(y, 4)
+xp, yp = gaussian_filter1d(x, int(sys.argv[2])), gaussian_filter1d(y, int(sys.argv[2]))
+#lp = gaussian_laplace(xy, 3)
+#print(lp)
+#xp, yp = lp[:, 0], lp[:, 1]
+#print(xp, yp)
+
 
 plt.plot(x, y)
 plt.plot(xp, yp)
@@ -19,6 +28,8 @@ plt.show()
 # replace columns
 df[0] = xp
 df[1] = yp
+
+df=df.drop(df.index[:7])
 
 print(df)
 
